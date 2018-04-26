217f1b217154c80e6d175cfc40b662dc0df7a6d1
Ali Can İPEK <alicanipek@outlook.com.tr>
2018-04-26 10:19:40 +0300

    added solution - Roman Numerals Encoder

--- Src/Lib/General.hs
+++ Src/Lib/General.hs
@@ -183,3 +183,14 @@ calc n | n < 10 = [n]
        
 toInt xs = (read . concatMap show) xs :: Integer
 
+-- Roman Numerals Encoder
+-- Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+romans = [(1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"), (40, "XL"), (50, "L"), (90, "XC"), (100, "C"), (400, "CD"), (500, "D"), (900, "CM"), (1000, "M")]
+
+solution :: Integer -> String
+solution 0 = []
+solution n | n `elem` map fst romans = snd (head (filter ((==n) . fst) romans))
+           | otherwise = snd (toRoman n) ++ solution (n - fst (toRoman n))
+           
+toRoman n = romans !! last (findIndices (<n) (map fst romans))
